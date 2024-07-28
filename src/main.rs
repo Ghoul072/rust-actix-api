77212bf65eab5df514e9a0266439783a26a905f6
Yaseen <ahm3d.yaseen@gmail.com>
2024-07-28 23:29:51 +0500

    Install dependencies and create .env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use dotenvy::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().expect(".env file not found");
+
+    let database_url = env::var("DATABASE_URL").expect("DATABSE_URL must be set");
+    println!("Database URL: {}", database_url);
+
+    let migrations_dir = env::var("MIGRATIONS_DIR").expect("MIGRATIONS_DIR must be set");
+    println!("Migrations Directory: {}", migrations_dir);
 }
